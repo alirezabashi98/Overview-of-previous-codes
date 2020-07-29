04be81b844d31932e61c8054c6f5c43b5b3b8f23
unknown <alirezsbashi@gmail.com>
2020-07-30 00:06:03 +0430

    add activity test edit text

---
+++ app/src/main/java/com/example/myapplication/TestEditText.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestEditText : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_edit_text)
+    }
+}
