1ef9b5acd7dc4d2c3f3a4ae3a4dedf393500fcb9
unknown <alirezsbashi@gmail.com>
2020-07-30 02:39:48 +0430

    edit activity_test_edittext + radiobutton + checkbox

---
+++ app/src/main/java/com/example/myapplication/testCheckBox.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class testCheckBox : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_check_box)
+    }
+}
