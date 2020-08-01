e656d1925eee1b98dc989a5410cb912372d3bf91
unknown <alirezsbashi@gmail.com>
2020-08-01 13:18:44 +0430

    end build activity_test_chronometer

--- app/src/main/java/com/example/myapplication/TestChronometer.kt
+++ app/src/main/java/com/example/myapplication/TestChronometer.kt
@@ -2,10 +2,30 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_test_chronometer.*
 
 class TestChronometer : AppCompatActivity() {
+
+    private var pauseTime = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_chronometer)
+
+        toggleBtn_Chronometer.setOnClickListener {
+            if (toggleBtn_Chronometer.isChecked){
+
+                test_Chronometer.base = SystemClock.elapsedRealtime() - pauseTime
+                test_Chronometer.start()
+
+            }else{
+
+                pauseTime = SystemClock.elapsedRealtime() - test_Chronometer.base
+                test_Chronometer.stop()
+
+            }
+        }
     }
 }
