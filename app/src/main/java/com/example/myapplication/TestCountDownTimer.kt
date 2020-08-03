cb1758d204c4295404f7a6f6072adf5811e0b8d4
unknown <alirezsbashi@gmail.com>
2020-08-03 12:52:48 +0430

    countDownTimer Profile

--- app/src/main/java/com/example/myapplication/TestCountDownTimer.kt
+++ app/src/main/java/com/example/myapplication/TestCountDownTimer.kt
@@ -2,10 +2,43 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_test_count_down_timer.*
+import kotlin.concurrent.timer
 
 class TestCountDownTimer : AppCompatActivity() {
+
+    var mainTime = 1_000L
+    var countDownTimer: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_count_down_timer)
+
+        toggle_countDownTimer.setOnClickListener {
+            if(toggle_countDownTimer.isChecked){
+                runTimer()
+            }else{
+                countDownTimer?.cancel()
+            }
+        }
+    }
+
+    fun runTimer() {
+        countDownTimer = object : CountDownTimer(mainTime, 1000) {
+            override fun onFinish() {
+
+            }
+
+            override fun onTick(timeOut: Long) {
+                mainTime = timeOut
+                setText(mainTime)
+            }
+
+        }
+    }
+
+    fun setText(s: Long) {
+        tv_countDownTimer.text = String.format("%02d:%02d", (s / 1000) / 60, (s / 1000) % 60)
     }
 }
