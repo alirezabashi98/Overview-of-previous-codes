ab8164871de5aab678c05d783553d0975990651f
unknown <alirezsbashi@gmail.com>
2020-08-05 17:49:04 +0430

    create activity_test_custom_listView

---
+++ app/src/main/java/com/example/myapplication/TestCustomListView.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestCustomListView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_custom_list_view)
+    }
+}
