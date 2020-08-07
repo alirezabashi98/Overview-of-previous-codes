f319ab226b996b5242ccf6390937e67ac1de2edb
unknown <alirezsbashi@gmail.com>
2020-08-07 18:34:00 +0430

    add custom spinner

--- app/src/main/java/com/example/myapplication/TestSpinner.kt
+++ app/src/main/java/com/example/myapplication/TestSpinner.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.myapplication.Adapter.adapter_listview
+import com.example.myapplication.DataClass.DataClass_ListView
 import kotlinx.android.synthetic.main.activity_test_spinner.*
 
 class TestSpinner : AppCompatActivity() {
@@ -16,5 +18,19 @@ class TestSpinner : AppCompatActivity() {
         )
 
         spinner.adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,list)
+
+//        val list = listOf(
+//            DataClass_ListView(R.drawable.img_login,"ali","hi"),
+//            DataClass_ListView(R.drawable.img_splsh_screen,"reza","hello"),
+//            DataClass_ListView(R.drawable.img_login,"ali","hi"),
+//            DataClass_ListView(R.drawable.img_login,"ali","hi"),
+//            DataClass_ListView(R.drawable.img_login,"ali","hi"),
+//            DataClass_ListView(R.drawable.img_login,"ali","hi"),
+//            DataClass_ListView(R.drawable.img_login,"ali","hi"),
+//            DataClass_ListView(R.drawable.img_login,"ali","hi")
+//        )
+//
+//
+//        spinner.adapter = adapter_listview(list)
     }
 }
