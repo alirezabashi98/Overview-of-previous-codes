3458e26a63fc455d1929635697b0c637df70fa2b
unknown <alirezsbashi@gmail.com>
2020-08-07 19:08:14 +0430

    end build activity test_AutoCompleteTextView

--- app/src/main/java/com/example/myapplication/TestAutoCompleteTextView.kt
+++ app/src/main/java/com/example/myapplication/TestAutoCompleteTextView.kt
@@ -2,10 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_test_auto_complete_text_view.*
 
 class TestAutoCompleteTextView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_auto_complete_text_view)
+
+        val list = listOf(
+            "ali",
+            "reza",
+            "mmd",
+            "sina"
+        )
+
+        test_AutoCompleteTextView.setAdapter(ArrayAdapter(this,android.R.layout.simple_list_item_1,list))
+        test_AutoCompleteTextView.threshold = 1
+
     }
 }
